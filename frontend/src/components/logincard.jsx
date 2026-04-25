2f5f71b163a0937fe6955c8b80c3014e700c40d2
kanika <sawantkannu024@gmail.com>
2026-04-26 02:32:27 +0530

    Update: Connected Login and Signup to live Render backend

--- frontend/src/components/logincard.jsx
+++ frontend/src/components/logincard.jsx
@@ -12,7 +12,7 @@ const LoginCard = ({ onLoginSuccess, onSwitchToSignup }) => {
     setError('');
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/login', {
+      const response = await fetch('https://studydashboard-qheu.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
--- frontend/src/components/signupcard.jsx
+++ frontend/src/components/signupcard.jsx
@@ -34,7 +34,7 @@ const SignupCard = ({ onSwitchToLogin }) => {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/signup', {
+      const response = await fetch('https://studydashboard-qheu.onrender.com/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
