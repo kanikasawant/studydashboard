d16f91e305901e65d6fb42a90645090856e13741
kanika <sawantkannu024@gmail.com>
2026-04-26 02:55:02 +0530

    Fixed CORS syntax

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://studydashboard-three.vercel.app/"
+        "https://studydashboard-three.vercel.app"
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:5174",
